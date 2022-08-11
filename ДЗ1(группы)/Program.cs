45081e025ddb23f77b11b3a6a0912fdb0e95f36f
Julia Nikolaeva <sunjshu@gmail.com>
2022-08-11 17:44:41 +0300

    homework

---
+++ ДЗ1(группы)/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+bool isSquare = b*b ==a ;
+if (isSquare==true) Console.WriteLine ("ДА");
+if (isSquare==false) Console.WriteLine ("НЕТ");
